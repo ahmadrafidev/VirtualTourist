ed2bc06b8661e408ad71434f1ed2335e5bb50e24
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-23 21:23:17 +0700

    Initial Commit

---
+++ VirtualTourist/VirtualTouristApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualTouristApp.swift
+//  VirtualTourist
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct VirtualTouristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
